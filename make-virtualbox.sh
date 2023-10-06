c649df8b2320121a8fac927c18476d23d902f2ad
William J. Bowman <wjb@williamjbowman.com>
2023-10-06 15:00:45 -0700

    Add virtualbox appliance and tweak script
    - Add a virtualbox appliance as OVF for easy import into VirtualBox
    - Tweak script to generate the format and name expected by OVF

--- make-virtualbox.sh
+++ make-virtualbox.sh
@@ -3,4 +3,4 @@
 # requires d2vm
 # https://github.com/linka-cloud/d2vm
 
-sudo d2vm build -p root -f Dockerfile -o wasm-prechk-artifact.vdi .
+sudo d2vm build -p root --keep-cache -f Dockerfile -o "POPL 2024 Artifact - Indexed Types for a Statically Safe WebAssembly-disk001.vmdk" .
